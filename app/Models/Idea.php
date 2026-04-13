43e3d881f06faee7bb6d3e653eee9c1c736fba97
Mouhamadou Moustapha Sissokho <siskomouhamed@gmail.com>
2026-04-13 21:10:14 +0000

    Fix type error

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -21,7 +21,7 @@ class Idea extends Model
     ];
 
     protected $attributes = [
-        'status' => IdeaStatus::PENDING,
+        'status' => IdeaStatus::PENDING->value,
     ];
 
     public function user(): BelongsTo
