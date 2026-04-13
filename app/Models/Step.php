cb2917cc5266148b961f5b004f417a872227eb7f
Mouhamadou Moustapha Sissokho <siskomouhamed@gmail.com>
2026-04-13 18:44:47 +0000

    Design the model layer

---
+++ app/Models/Step.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\StepFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Step extends Model
+{
+    /** @use HasFactory<StepFactory> */
+    use HasFactory;
+
+    protected $attributes = [
+        'completed' => false,
+    ];
+
+    public function idea(): BelongsTo
+    {
+        return $this->belongsTo(Idea::class);
+    }
+}
